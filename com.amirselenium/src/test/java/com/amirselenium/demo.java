b7b151ed9ca37247011b864eca36430cd0ea58ae
Jyotiranjan <Jyotiranjan@Dell>
2018-10-31 16:42:23 +0530

    Full commit

---
+++ com.amirselenium/src/test/java/com/amirselenium/demo.java
@@ -0,0 +1,13 @@
+package com.amirselenium;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test
+	public void qwerty()
+	{
+		System.out.println("baaaaaaaaaaaaaaaaaaandaaaaa");
+	}
+
+}
